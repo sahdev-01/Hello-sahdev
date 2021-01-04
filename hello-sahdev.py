9e3109f7a00a4d2e90852b74d2b189b43af58d7c
sahdev-01 <sahdevdon.singh1@gmail.com>
2021-01-04 13:31:02 +0530

    First project on Github by Sahdev Singh

--- hello-sahdev.py
+++ hello-sahdev.py
@@ -1,1 +1,1 @@
-print("Hello Sahdev, This is a first project on Github \n Hope you like it !!!")
+print("Hello Everyone, \nThis is a first project on Github \nHope you like it !!! \nBy Sahdev Singh")
