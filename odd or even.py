3c7ca0c49816d40e21cab7a65c368537dd8ee8a9
Sahdev Singh <sahdevdon.singh1@gmail.com>
2021-01-06 14:37:12 +0530

    Identify ODD or EVEN number entered by a user.

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+# To find odd or even number entered by a user
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print(number, "is even number.")
+else:
+    print(number, "is odd number.")
