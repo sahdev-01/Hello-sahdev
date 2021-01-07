e02bd809dfa214d07b21cef5531d503c581e9881
Sahdev Singh <sahdevdon.singh1@gmail.com>
2021-01-07 21:29:50 +0530

    User defined(number till) prime numbers to be printed.

--- prime.py
+++ prime.py
@@ -1,8 +1,8 @@
 number = int(input("Enter number(till) prime numbers to be printed: "))
 for n in range(1, number):
     flag = True
-    for i in range(2,n):
-        if n % i ==0:
+    for i in range(2, n):
+        if n % i == 0:
             flag = False
             break
     if flag:
